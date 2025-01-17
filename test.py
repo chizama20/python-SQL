ee0fd3682c89512510bb24884d087ab3feb8cded
chizama20 <133240770+chizama20@users.noreply.github.com>
2025-01-17 04:22:04 -0500

    new

--- test.py
+++ test.py
@@ -1,1 +1,38 @@
-print("Hello World")
+import psycopg2
+
+hostname ='localhost'
+database = 'demoDB'
+username = 'postgress'
+pwd = 'Mambo651923'
+port_id = 5432
+
+conn, cur = None, None
+try:
+    conn = psycopg2.connect(
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = pwd,
+            port = port_id
+    )
+
+    cur = conn.cursor()
+
+    create_script = ''' CREATE TABLE IF NOT EXISTS employee (
+                            id      int  PRIMARY KEY,
+                            name    varchar(40) NOT NULL,   
+                            salary  int
+                            dept_id varchar(30))  '''           
+                                            
+    cur.execute(create_script)
+
+
+    conn.commit()
+
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
